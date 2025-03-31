7c5edc0288f3ceebf3696cd1bee6962255106675
BlueSparrow64 <b_t_s420@hotmail.com>
2025-03-31 16:56:07 -0400

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -3,8 +3,8 @@ from instapy import InstaPy
 from instapy import smart_run
 
 # login credentials
-insta_username = ''
-insta_password = ''
+insta_username = 'digitalnetworkingai'
+insta_password = 'Brotherai12'
 
 comments = ['Nice shot! @{}',
         'I love your profile! @{}',
